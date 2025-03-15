76ddc1a5dd30d59420ce95cef5c9278035ebb588
masayasviel <33654883+masayasviel@users.noreply.github.com>
2025-03-15 22:36:33 +0900

    wip

--- src/app/internal/attendance-sheet/attendance-sheet.component.ts
+++ src/app/internal/attendance-sheet/attendance-sheet.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { MemberAttendanceRecord } from '@/api/attendance.service';
+import { InternalFacade } from '@/internal/store/internal.facade';
+import { Component, computed, effect, inject } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-attendance-sheet',
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './attendance-sheet.component.html',
   styleUrl: './attendance-sheet.component.scss',
 })
-export class AttendanceSheetComponent {}
+export class AttendanceSheetComponent {
+  private readonly internalFacade = inject(InternalFacade);
+
+  record = this.internalFacade.signalRecord;
+  displayColumns = computed(() => ['name', ...(this.record()?.candidateDate ?? [])]);
+
+  dataSource = new MatTableDataSource<MemberAttendanceRecord>();
+
+  constructor() {
+    effect(() => {
+      this.dataSource.data = this.record()?.records ?? [];
+    });
+  }
+}
