076428b1ac4ee49ac99bd5bf6f0f811a09a02155
masayasviel <33654883+masayasviel@users.noreply.github.com>
2025-03-15 15:59:38 +0900

    :sparkles: action

---
+++ src/app/internal/store/internal.action.ts
@@ -0,0 +1,27 @@
+import { GetAttendanceTableResponse, MemberAttendanceRecord } from '@/api/attendance.service';
+import { createAction, props } from '@ngrx/store';
+
+export const loadRecords = createAction('[Attendance Sheet] Load Records', props<{ uuid: string }>());
+export const loadRecordsSuccess = createAction(
+  '[Attendance Sheet] Load Records Success',
+  props<{ response: GetAttendanceTableResponse }>(),
+);
+export const registerAttendance = createAction(
+  '[Attendance Sheet] Register Attendance',
+  props<{ title: string; candidateDate: string[] }>(),
+);
+export const deleteAttendance = createAction('[Attendance Sheet] Delete Attendance', props<{ uuid: string }>());
+export const registerUserAttendance = createAction(
+  '[Attendance Sheet] Register User Attendance',
+  props<{ uuid: string; newAttendance: MemberAttendanceRecord }>(),
+);
+export const updateUserAttendance = createAction(
+  '[Attendance Sheet] Update User Attendance',
+  props<{ uuid: string; name: string; status: MemberAttendanceRecord['status'] }>(),
+);
+export const deleteUserAttendance = createAction(
+  '[Attendance Sheet] Delete User Attendance',
+  props<{ uuid: string; name: string }>(),
+);
+export const success = createAction('[Attendance Sheet] Success');
+export const failed = createAction('[Attendance Sheet] Failed');
